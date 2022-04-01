05b168622ef361ca138d472498876a7716dcdc58
jolieleung <102486311+jolieleung@users.noreply.github.com>
2022-04-01 10:35:56 -0400

    depreciation executable_path

--- scrape_test.py
+++ scrape_test.py
@@ -1,12 +1,18 @@
 
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+s = Service("/Users/jolieleung/Documents/GitHub/pfch_finalproject/chromedriver") 
 
 from selenium.webdriver import ChromeOptions, Chrome
 opts = ChromeOptions()
 opts.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=opts, executable_path=r"C:\Users\leung\Downloads\chromedriver_win32\chromedriver.exe")
+#driver = webdriver.Chrome(options=opts, executable_path="./chromedriver")
+driver = webdriver.Chrome(options=opts, service=s)
 
 driver.get('https://www.google.com')
+
+
+
 
